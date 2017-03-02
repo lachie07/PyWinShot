b4d3690b15207736fa08b23f9d3feee4263aa34f
lachie07 <lachie07@gmail.com>
2017-03-02 12:35:14 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PyWinShot
+A python script that will take a screenshot and save it on Windows.
+
+**Current version:** 1.0
+
+**Setup**   
+1. Download .zip of script  
+2. Extract script from .zip   
+3. Run script and wait 5 seconds for screenshot to take   
+
+**Usage**   
+When the script is run, it will give you a count down of 5 seconds (this can be changed, more info further below). Once the 5 seconds is up, it will take a screenshot and then open the screenshot for you to see.   
+
+**Settings**   
+It is possible to change a few of the settings, currently you can change: 
+* Save Directory  
+* Timer countdown   
+
+**To Do**
+* (Optional) Upload the screenshot to an image hosting service (or server)    
---
+++ screenshot.py
@@ -0,0 +1,29 @@
+import os
+import time
+import ImageGrab
+
+#------------------
+SAVE_DIRECTORY = "C:\\Users\\Lachie\\Desktop\\"
+TIMER_COUNTDOWN = 5
+#------------------
+
+print """Note:
+When the screenshot is taken, it will display it in a low resolution.
+This problem cannot be fixed, so deal with it. To view a higher
+resolution image, simply open up the picture via Windows Explorer.
+To change the save directory, simply open up the script and change
+"Save Directory" to where ever you want it (putting a double '\'
+instead of a single one)."""
+
+time.sleep(4)
+
+for i in range(TIMER_COUNTDOWN,-1,-1):
+    print i
+    time.sleep(1)
+
+img = ImageGrab.grab()
+saveas = os.path.join(SAVE_DIRECTORY,"Screenshot.jpg")
+img.show()
+option = raw_input("Would you like to save this screenshot(Y/N): ")
+if option.lower() == "y":
+    img.save(saveas)
